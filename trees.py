b823fcfe7deeeb694331e886ced67dce4ad708db
Sheik Anik <anik801@yahoo.com>
2025-09-24 16:24:32 -0500

    BST Update

--- trees.py
+++ trees.py
@@ -35,19 +35,38 @@ def searchTree(root, key):
     findRight = searchTree(root.right, key)
     return findLeft or findRight
 
+def getHeight(root, lvl):
+    if not root: return 0
+    leftLvl = getHeight(root.left, lvl)
+    rightLvl = getHeight(root.right, lvl)
+    return 1 + max(leftLvl, rightLvl)
+
+def countNodes(root):
+    # TODO: write your code here
+    pass
+
 
 # Execution
 # arr = [6, 4, 5, 3, 1, 12, 8, 9, 2, 14]
-arr = [5, 2, 8]
+arr = [13, 10, 7, 12, 11, 26, 20]
+# arr = [5, 2, 8]
 
 # Create a tree from the given array
 root = TreeNode(arr[0])
 for val in arr[1:]:
     insertNode(root, val)
     
-inOrder(root)
+# Print all elements
+# inOrder(root)
 
 # Search operations
-print(searchTree(root, 17))
-print(searchTree(root, 5))
-print(searchTree(root, 2))
+# print(searchTree(root, 17))
+# print(searchTree(root, 5))
+# print(searchTree(root, 2))
+
+height = getHeight(root, 0)
+print("Height: ", height)
+
+count = 0
+countNodes(root)
+print(count == len(arr)) # Correct: True otherwise False
